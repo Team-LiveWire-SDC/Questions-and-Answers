814d5383b1f998bfd88e4d67837c45524e56e3b9
Elliot Landon <elliotlandon5@gmail.com>
2022-04-26 14:33:45 -0700

    host

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'superuser',
-  host: '204.236.138.189',
+  host: '172.31.30.27',
   database: 'questionsandanswers2',
   password: '1234',
   port: 5432,
